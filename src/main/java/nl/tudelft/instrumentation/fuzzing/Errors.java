118b0413b72a5f8b849de1f23df8a44aa11681aa
schorliee <da.charlie.nguyen@gmail.com>
2021-02-23 21:49:02 +0100

    print output in error class to track errors

--- src/main/java/nl/tudelft/instrumentation/fuzzing/Errors.java
+++ src/main/java/nl/tudelft/instrumentation/fuzzing/Errors.java
@@ -1,9 +1,19 @@
 package nl.tudelft.instrumentation.fuzzing;
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
 
 public class Errors {
+    static HashSet<String> allErrors = new HashSet<>();
 
     public static void __VERIFIER_error(int i) {
         //throw new IllegalStateException( "error_" + i );
-        System.out.println("error_" + i);
+        String errorString = ("error_" + i);
+        System.out.println(errorString);
+        allErrors.add(errorString);
+        System.out.println(allErrors);
+
     }
 }
