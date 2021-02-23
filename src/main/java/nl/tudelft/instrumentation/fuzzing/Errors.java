a6c69ad7b21818f08b4092592ef8579589a6a281
schorliee <da.charlie.nguyen@gmail.com>
2021-02-23 21:20:41 +0100

    change the error class from throwing to print

--- src/main/java/nl/tudelft/instrumentation/fuzzing/Errors.java
+++ src/main/java/nl/tudelft/instrumentation/fuzzing/Errors.java
@@ -3,6 +3,7 @@ package nl.tudelft.instrumentation.fuzzing;
 public class Errors {
 
     public static void __VERIFIER_error(int i) {
-        throw new IllegalStateException( "error_" + i );
+        //throw new IllegalStateException( "error_" + i );
+        System.out.println("error_" + i);
     }
 }
