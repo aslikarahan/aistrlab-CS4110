5fa29a67a8c8f1f5adbedbd6843e8d17228bacff
schorliee <da.charlie.nguyen@gmail.com>
2021-03-04 23:49:22 +0100

    compile and run file

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
-x=11
-while [ $x -le 16 ]
-do
-  java -cp target/aistr.jar:. Problem$x
-  x=$(( $x + 1 ))
-done
+mvn clean package
+
+java -cp target/aistr.jar nl.tudelft.instrumentation.Main --type=symbolic --file=../RERS/Problem1/Problem1.java > Problem1.java
+
+
+javac -cp target/aistr.jar:lib/com.microsoft.z3.jar:. Problem1.java
+
+java -cp target/aistr.jar:lib/com.microsoft.z3.jar:Problem1:. Problem1
